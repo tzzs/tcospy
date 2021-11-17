26d4487707eac10f80d30559a253c78e83f0ebce
TZZ <1789803837@qq.com>
2021-11-18 01:07:42 +0800

    add tcosupload app

--- tcospy/settings.py
+++ tcospy/settings.py
@@ -37,13 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -123,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOW_ORGINS = [
+#     '*'
+# ]
+CORS_ORIGIN_ALLOW_ALL = True
--- tcosupload/views.py
+++ tcosupload/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import UploadFileForm
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("hello world")
+
+
+def upload(request):
+    print(request.FILES.get('file'))
+    if request.method == 'POST':
+        file = request.FILES.get('file')
+        with open(file.name, 'wb') as f:
+            f.write(file.read())
+            f.close()
+        # todo upload files to tencent cos
+        msg = 'successful'
+    else:
+        msg = 'request method error'
+    return HttpResponse(msg)
